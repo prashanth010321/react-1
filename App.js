94ace641b89028d9c04aa0d2c3a99984fd16f11f
prashanth010321 <116644292+prashanth010321@users.noreply.github.com>
2023-04-14 19:17:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm'
+
+
+function App() {
+  return (
+    <>
+    <Navbar title="project" About="About Project"/> 
+    <div className='container my-3'>
+    <TextForm  heading="Enter the text to analyze"/>
+    </div>
+    
+    </>
+    
+  );
+}
+
+export default App;
